c220d7aafaab7288bba90a951d734217ff074886
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-10-16 22:51:33 +0200

    chore: přidány složky shared a features do konfigurace a upraveno nastavení container v tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,34 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './shared/**/*.{js,ts,jsx,tsx,mdx}',
+    './features/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '1rem',
+          md: '1.5rem',
+          lg: '2rem',
+          xl: '3rem',
+        },
+        screens: {
+          sm: '100%',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',
+          '2xl': '1440px',
+        },
+      },
+      maxWidth: {
+        '2xl': '90%',
+        '3xl': '1600px',
+      },
+    },
   },
   plugins: [],
 };
