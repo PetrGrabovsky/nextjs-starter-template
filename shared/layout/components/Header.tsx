833e5f425d543c1692ab8c78588dcfe344cd5dcf
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-10-17 23:57:39 +0200

    feat: do Header přidáno podmíněné zobrazení Navigation nebo tlačítka na základě isMobileView stavu

--- shared/layout/components/Header.tsx
+++ shared/layout/components/Header.tsx
@@ -1,12 +1,17 @@
+'use client';
+
 import { FC } from 'react';
 import Navigation from './Navigation';
+import { useAppSelector } from '@/store/hooks';
 
 const Header: FC = () => {
+  const isMobileView = useAppSelector((state) => state.layout.isMobileView);
+
   return (
     <header className='flex h-14 items-center justify-center bg-blue-600'>
       <div className='container flex items-center justify-between'>
         <span>Logo</span>
-        <Navigation />
+        {isMobileView ? <button className='bg-red-800'>MENU</button> : <Navigation />}
       </div>
     </header>
   );
